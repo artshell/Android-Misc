8119c8d485912c3ba7035232f18dda0c1e952e2b
artshell <872780008.qq.com>
2018-03-13 23:49:33 +0800

    Update: configuration

--- databinding/src/main/java/com/artshell/databinding/MainActivity.java
+++ databinding/src/main/java/com/artshell/databinding/MainActivity.java
@@ -1,7 +1,7 @@
-package com.artshell.arch;
+package com.artshell.databinding;
 
-import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
---
+++ mvp/src/main/java/com/artshell/mvp/app/AppContext.java
@@ -0,0 +1,24 @@
+package com.artshell.mvp.app;
+
+import android.app.Application;
+
+import com.squareup.leakcanary.LeakCanary;
+
+/**
+ * @author artshell on 31/01/2018
+ */
+
+public class AppContext extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        // 内存泄漏检测
+        if (LeakCanary.isInAnalyzerProcess(this)) {
+            // This process is dedicated to LeakCanary for heap analysis.
+            // You should not init your app in this process.
+            return;
+        }
+        LeakCanary.install(this);
+    }
+}
