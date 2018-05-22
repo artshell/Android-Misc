0d552bdafc9b0a19f5a51592082430f4811d1096
wujiefeng <artshellfish@gmail.com>
2018-05-22 17:47:14 +0800

    Add: some sample"

--- app/src/main/java/com/artshell/misc/service/ReactiveServiceActivity.java
+++ app/src/main/java/com/artshell/misc/service/ReactiveServiceActivity.java
@@ -1,10 +1,82 @@
 package com.artshell.misc.service;
 
+import android.annotation.SuppressLint;
+import android.app.Service;
+import android.content.ComponentName;
+import android.content.Intent;
+import android.content.ServiceConnection;
+import android.os.Bundle;
+import android.os.IBinder;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+
+import com.artshell.misc.R;
+
+import java.util.Arrays;
 
 /**
  * @author artshell on 2018/5/20
  */
-public class ReactiveServiceActivity extends AppCompatActivity {
+public class ReactiveServiceActivity extends AppCompatActivity implements View.OnClickListener {
 
+    private static final String TAG = "ReactiveServiceActivity";
+    private ServiceConnection conn;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedState) {
+        super.onCreate(savedState);
+        setContentView(R.layout.activity_rx_live_service);
+
+        findViewById(R.id.btn_obtain).setOnClickListener(this);
+        findViewById(R.id.btn_unbind).setOnClickListener(v -> {
+            if (conn != null) {
+                // 解绑, 验证"延时10秒的任务"是否能收到结果
+                unbindService(conn);
+                conn = null;
+            }
+        });
+    }
+
+    @Override
+    public void onClick(View v) {
+        conn = new ServiceConnection() {
+
+            @SuppressLint("CheckResult")
+            @Override
+            public void onServiceConnected(ComponentName name, IBinder service) {
+                RxLiveDataService.SourceBinder binder = (RxLiveDataService.SourceBinder) service;
+
+                // 延时10秒
+                Bundle reactArgs = new Bundle();
+                reactArgs.putString(RxLiveDataService.PARAMETER_KEY, "React");
+                binder.getPublisherSource(reactArgs)
+                        .subscribe(result -> Log.i(TAG, "PublisherSource: " + Arrays.toString(result)));
+
+                // 延时5秒
+                Bundle liveArgs = new Bundle();
+                liveArgs.putString(RxLiveDataService.PARAMETER_KEY, "Live");
+                binder.getLiveSource(liveArgs)
+                        .observe(ReactiveServiceActivity.this, items -> Log.i(TAG, "LiveSource: " + items));
+            }
+
+            @Override
+            public void onServiceDisconnected(ComponentName name) {
+
+            }
+        };
+
+        Intent tent = new Intent(this, RxLiveDataService.class);
+        bindService(tent, conn, Service.BIND_AUTO_CREATE);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (conn != null) {
+            unbindService(conn);
+            conn = null;
+        }
+    }
 }
--- app/src/main/java/com/artshell/misc/service/RxLiveDataService.java
+++ app/src/main/java/com/artshell/misc/service/RxLiveDataService.java
@@ -1,18 +1,115 @@
 package com.artshell.misc.service;
 
+import android.annotation.SuppressLint;
+import android.arch.lifecycle.Lifecycle;
+import android.arch.lifecycle.LiveData;
+import android.arch.lifecycle.MutableLiveData;
+import android.content.Intent;
 import android.os.Binder;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.HandlerThread;
+import android.os.IBinder;
+import android.os.Looper;
+import android.support.annotation.Nullable;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import io.reactivex.Observable;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.schedulers.Schedulers;
+import io.reactivex.subjects.PublishSubject;
+import io.reactivex.subjects.Subject;
 
 /**
  * @author artshell on 2018/5/21
  */
 public class RxLiveDataService extends ReactiveService {
+    private static final String TAG = "RxLiveDataService";
+
+    public static final String PARAMETER_KEY = "parameter_key";
+
+    private volatile Looper  mLooper;
+    private volatile Handler mHandler;
+
+    private MutableLiveData<List<String>> mLiveSource = new MutableLiveData<>();
+    private Subject<String[]>             mPublisher  = PublishSubject.<String[]>create().toSerialized();
+    private SourceBinder                  mBinder     = new SourceBinder();
 
     public class SourceBinder extends Binder {
 
-        RxLiveDataService getService() {
+        public RxLiveDataService getService() {
             return RxLiveDataService.this;
         }
 
+        public LiveData<List<String>> getLiveSource(Bundle args) {
+            return RxLiveDataService.this.LiveSource(args);
+        }
 
+        public Observable<String[]> getPublisherSource(Bundle args) {
+            return RxLiveDataService.this.getPublisherSource(args)
+                    .compose(bindUntilEvent(Lifecycle.Event.ON_STOP));
+        }
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        HandlerThread thread = new HandlerThread("RxLiveDataService");
+        thread.start();
+
+        mLooper = thread.getLooper();
+        mHandler = new Handler(mLooper);
+    }
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        super.onBind(intent);
+        return mBinder;
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        if (mLooper != null) {
+            mLooper.quit();
+        }
+    }
+
+    /**
+     * 延时5秒
+     * @param args
+     * @return
+     */
+    private LiveData<List<String>> LiveSource(Bundle args) {
+        String key = args.getString(PARAMETER_KEY);
+        mHandler.postDelayed(() -> {
+            List<String> asList = Arrays.asList("liveData-" + key, "lifecycle-" + key, "Room-" + key, "Paging-" + key);
+            mLiveSource.postValue(asList);
+        }, 5000);
+        return mLiveSource;
+    }
+
+
+    /**
+     * 延时10秒
+     * @param args
+     * @return
+     */
+    @SuppressLint("CheckResult")
+    private Observable<String[]> getPublisherSource(Bundle args) {
+        String key = args.getString(PARAMETER_KEY);
+        Observable.fromArray("A", "B", "C", "D", "E", "F")
+                .collectInto(new ArrayList<String>(), (list, item) -> list.add(item + "-" + key))
+                .delay(10000, TimeUnit.MILLISECONDS)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(arr -> mPublisher.onNext(arr.toArray(new String[arr.size()])), throwable -> {
+                });
+        return mPublisher;
     }
 }
